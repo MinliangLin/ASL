beda0989a8e30ac51a7ce9f9e247a12bbe84ec96
Minliang <minliang.lin@hotstar.com>
2021-12-15 11:38:38 +0000

    fix typo

--- test.py
+++ test.py
@@ -17,7 +17,7 @@ parser.add_argument('--model_name', type=str, default='tresnet_l')
 parser.add_argument('--input_size', type=int, default=448)
 parser.add_argument('--dataset_type', type=str, default='OpenImages')
 parser.add_argument('--th', type=float, default=None)
-parser.add_argument('--show', default=False)
+parser.add_argument('--show', action="store_true", default=False)
 parser.add_argument('--out', default=None)
 
 
@@ -39,10 +39,11 @@ def main():
 
     # doing inference
     print('loading image and doing inference...')
-    start = time.time()
+    start = time()
+    globs = sorted(glob(args.glob))
     
-    for path in glob(args.glob):
-        im = Image.open(args.pic_path)
+    for path in globs:
+        im = Image.open(path)
         im_resize = im.resize((args.input_size, args.input_size))
         np_img = np.array(im_resize, dtype=np.uint8)
         tensor_img = torch.from_numpy(np_img).permute(2, 0, 1).float() / 255.0  # HWC to CHW
@@ -50,23 +51,11 @@ def main():
         output = torch.squeeze(torch.sigmoid(model(tensor_batch)))
         np_output = output.cpu().detach().numpy()
         detected_classes = classes_list[np_output > args.th]
+        if args.show:
+            print(path, f'{list(detected_classes)=}')
     
-    print(f'time: {time.time()-start}')
+    print(f'time: {time()-start}')
     print('done\n')
-    if args.show:
-        print(f'{detected_classes=}')
-
-    # example loss calculation
-    
-    output = model(tensor_batch)
-    loss_func1 = AsymmetricLoss()
-    loss_func2 = AsymmetricLossOptimized()
-    target = output.clone()
-    target[output < 0] = 0  # mockup target
-    target[output >= 0] = 1
-    loss1 = loss_func1(output, target)
-    loss2 = loss_func2(output, target)
-    assert abs((loss1.item() - loss2.item())) < 1e-6
 
 if __name__ == '__main__':
     main()
